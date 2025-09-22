c8b0a47aab168893323d9c1ab325f3abfad95478
Melonshark1 <quinnpeers@seattleacademy.org>
2025-09-22 19:15:58 +0000

    done

---
+++ Projects/quiz.py
@@ -0,0 +1,29 @@
+roblox_points = 0
+minecraft_points = 0
+block = input("blocks or studs ")
+if block == block:
+    minecraft_points += 1
+elif block == "studs":
+    roblox_points += 1
+animal = input("cats or dogs")
+if animal == "dog" or "puppy":
+    print ("woof")
+    minecraft_points += 1
+elif animal == "cat":
+    print ("meow")
+    roblox_points += 1
+mental = input("do you like to create or to exsplore ")
+if mental == "exsplore":
+    roblox_points += 1
+elif mental == "create":
+    minecraft_points += 1
+animal = input("crafting or taking")
+if animal == "crafting":
+    minecraft_points += 1
+elif animal == "taking":
+    roblox_points += 1
+animal = input("r or m")
+if animal == "m or M":
+    minecraft_points += 1
+elif animal == "r or R":
+    roblox_points += 1
