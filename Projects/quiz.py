aa07567ade30e9a70fa733ce99d15e25f1e24ce7
Melonshark1 <quinnpeers@seattleacademy.org>
2025-10-08 17:51:42 +0000

    ok

--- Projects/quiz.py
+++ Projects/quiz.py
@@ -1,29 +1,42 @@
 roblox_points = 0
 minecraft_points = 0
 block = input("blocks or studs ")
-if block == block:
+if block == "blocks":
     minecraft_points += 1
 elif block == "studs":
     roblox_points += 1
+print (f"roblox = {roblox_points}")
+print (f"minecraft{ minecraft_points}")
 animal = input("cats or dogs")
-if animal == "dog" or "puppy":
-    print ("woof")
+if animal == "dogs" or animal == "puppy":
     minecraft_points += 1
-elif animal == "cat":
-    print ("meow")
+elif animal == "cats":
     roblox_points += 1
-mental = input("do you like to create or to exsplore ")
-if mental == "exsplore":
+print (f"roblox = {roblox_points}")
+print (f"minecraft{ minecraft_points}")
+mental = input("do you like to create or to explore ")
+if mental == "explore":
     roblox_points += 1
 elif mental == "create":
     minecraft_points += 1
+print (f"roblox = {roblox_points}")
+print (f"minecraft{ minecraft_points}")
 animal = input("crafting or taking")
 if animal == "crafting":
     minecraft_points += 1
 elif animal == "taking":
     roblox_points += 1
-animal = input("r or m")
-if animal == "m or M":
+print (f"roblox = {roblox_points}")
+print (f"minecraft{ minecraft_points}")
+animal = input("r or m ")
+if animal == "m":
     minecraft_points += 1
-elif animal == "r or R":
+elif animal == "r ":
     roblox_points += 1
+print (f"roblox = {roblox_points}")
+print (f"minecraft{ minecraft_points}")
+if roblox_points > minecraft_points:
+    print ("you like roblox better.")
+elif roblox_points < minecraft_points:
+    print ("you like minecraft better")
+elif roblox_points and 1 == 2 print("wow")
